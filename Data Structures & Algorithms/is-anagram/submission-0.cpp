79e59771dcbc8757ecd16de44458c26ac84b2856
Aashish Mishra <aashish.mishra1002@gmail.com>
2026-08-20 19:48:21 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        vector<int>freq(26);
+
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        for(char c:t){
+            freq[c-'a']--;
+        }
+
+        for(int fr:freq){
+            if(fr!=0) return false;
+        }
+        return true;
+    }
+
+};
